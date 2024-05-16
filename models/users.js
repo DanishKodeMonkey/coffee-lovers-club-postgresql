2e3451fe5c1b8b1b0ed6e10f679aa8b8ad2683cf
danishkodemonkey <humberjini@hotmail.com>
2024-05-16 12:33:04 +0200

    feat: create users schema and model

---
+++ models/users.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const UsersSchema = new Schema({
+    username: { type: String, required: true, maxLength: 100 },
+    first_name: { type: String, required: true, maxLength: 200 },
+    last_name: { type: String, required: true, maxLength: 200 },
+    membership: {
+        type: String,
+        required: true,
+        enum: ['Guest', 'Member', 'Admin'],
+        default: 'Guest',
+    },
+    password: { type: String, required: true },
+    // Refer to users messages
+    messages: [{ type: Schema.Types.ObjectId, ref: 'Messages' }],
+});
+
+UsersSchema.virtual('full_name').get(function () {
+    // Avoid errors in cases where first or last name is somehow not existant
+    let fullname = '';
+    if (this.first_name && this.last_name) {
+        fullname = `${this.first_name}, ${this.last_name}`;
+    }
+
+    return fullname;
+});
+
+UsersSchema.virtual('url').get(function () {
+    return `/club/users/${this._id}`;
+});
+
+module.exports = mongoose.model('Users', UsersSchema);
