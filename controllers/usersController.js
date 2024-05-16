cba40271e28e2b22947b1a256d9bb939b65da561
danishkodemonkey <humberjini@hotmail.com>
2024-05-16 16:22:36 +0200

    wip: user sign-up get and post

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,5 +1,6 @@
 const Users = require('../models/users');
 const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator');
 
 // Password stuff
 const passport = require('passport');
@@ -11,9 +12,33 @@ exports.sign_up_get = asyncHandler(async (req, res, next) => {
 });
 
 // sign up form POST
-exports.sign_up_post = asyncHandler(async (req, res, next) => {
-    res.send('Not implemented: user sign up POST');
-});
+exports.sign_up_post = [
+    // validate and sanitize input
+    body('username', 'Username must be specified')
+        .trim()
+        .isLength({ min: 1, max: 100 })
+        .withMessage('Keep username length to 100 characters or below')
+        .escape(),
+    body('firstName', 'First name must be specified')
+        .trim()
+        .isLength({ min: 1, max: 200 })
+        .withMessage('Keep first name length to 200 characters or below')
+        .escape(),
+    body('lastName', 'Last name must be specified')
+        .trim()
+        .isLength({ min: 1, max: 200 })
+        .withMessage('Keep last name length to 200 characters or below')
+        .escape(),
+    body('password', 'A password is required')
+        .trim()
+        .isLength({ min: 4, max: 9999 })
+        .withMessage(
+            'Please ensure passwords are between 4 and 9999 characters.'
+        )
+        .escape(),
+
+    // process request after validation and sanitization
+];
 
 // sign in form GET
 exports.sign_in_get = asyncHandler(async (req, res, next) => {
