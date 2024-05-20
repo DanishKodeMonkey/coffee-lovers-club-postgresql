2791205076c1a157e8906d2fa11debffc93f8be3
danishkodemonkey <humberjini@hotmail.com>
2024-05-20 12:38:06 +0200

    feat: add ensureAuthenticated custom middleware to all messageboard routes

--- routes/messageboard.js
+++ routes/messageboard.js
@@ -1,32 +1,58 @@
 const express = require('express');
+const ensureAuthenticated =
+    require('../middleware/authMiddleware').ensureAuthenticated;
 const router = express.Router();
 
 // require  controller modules
 const messages_controller = require('../controllers/messagesController');
 
 /// index route ///
-router.get('/', messages_controller.index);
+router.get('/', ensureAuthenticated, messages_controller.index);
 
 /// MESSAGES ROUTES ///
 // GET request for create message
-router.get('/messages/create', messages_controller.message_create_get);
+router.get(
+    '/messages/create',
+    ensureAuthenticated,
+    messages_controller.message_create_get
+);
 
 // POST request for create message
-router.get('/messages/create', messages_controller.message_create_post);
+router.get(
+    '/messages/create',
+    ensureAuthenticated,
+    messages_controller.message_create_post
+);
 
 // GET request for delete message
-router.get('/messages/:id/delete', messages_controller.message_delete_get);
+router.get(
+    '/messages/:id/delete',
+    ensureAuthenticated,
+    messages_controller.message_delete_get
+);
 
 // POST request for delete message
-router.get('/messages/:id/delete', messages_controller.message_delete_post);
+router.get(
+    '/messages/:id/delete',
+    ensureAuthenticated,
+    messages_controller.message_delete_post
+);
 
 // GET request for update message
-router.get('/messages/:id/update', messages_controller.message_update_get);
+router.get(
+    '/messages/:id/update',
+    ensureAuthenticated,
+    messages_controller.message_update_get
+);
 
 // POST request for update message
-router.get('/messages/:id/update', messages_controller.message_update_post);
+router.get(
+    '/messages/:id/update',
+    ensureAuthenticated,
+    messages_controller.message_update_post
+);
 
 // GET request for all messages
-router.get('/messages', messages_controller.messages_list);
+router.get('/messages', ensureAuthenticated, messages_controller.messages_list);
 
 module.exports = router;
